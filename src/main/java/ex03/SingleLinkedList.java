ba0177466806640e34db8516902c75a3d41eaa9f
MJ-0701 <jack2718@naver.com>
2022-05-12 14:52:10 +0900

    LinkedList02

--- src/main/java/ex03/SingleLinkedList.java
+++ src/main/java/ex03/SingleLinkedList.java
@@ -1,10 +1,14 @@
 package ex03;
 
+import java.util.Date;
+
+
+
 public class SingleLinkedList<T> {
 
     public Node<T> head = null;
 
-    public class Node<T>{
+    public class Node<T> {
         T data;
         Node<T> next = null;
 
@@ -16,14 +20,74 @@ public class SingleLinkedList<T> {
 
     public void addNode(T data){
         if(head == null){
-            head = new Node(data);
+            head = new Node<T>(data);
         }
         else{
             Node<T> node = this.head;
-            while(node != null){ // 가장 최근에 추가된 노드를 가르키게 하는 와일구문
+            while(node.next != null){ // 가장 최근에 추가된 노드를 가르키게 하는 와일구문
                 node = node.next;
             }
             node.next = new Node<T>(data); // 현재 추가된 노드를 마지막으로 보내는 구문
+        }
+    }
+
+    public void printAll(){
+        if(head != null){
+            Node<T> node = this.head;
+            System.out.println(node.data);
+            while(node.next != null){
+                node = node.next;
+                System.out.println(node.data);
+            }
+        }
+    }
+
+    public Node<T> search(T data){
+        if(this.head == null){
+            return null;
+        }else{
+            Node<T> node = this.head;
+            while (node != null){
+                if(node.data == data){
+                    return node;
+                }else{
+                    node = node.next;
+                }
+            }
+            return null;
+        }
+    }
+
+    public void addNodeInside(T data, T isData){
+        Node<T> searchedNode = this.search(isData);
+
+        if(searchedNode == null){
+            this.addNode(data);
+        }else{
+            Node<T> nextNode = searchedNode.next;
+            searchedNode.next = new Node<T>(data); // 중간에 낀(새로 넣은 노드의 주소를 가르켜야 됨)
+            searchedNode.next.next = nextNode; // 기존에 가르키던 주소는 뒤로 밀렸으므로 가운데 낀 노드의 포인터 값이 돼야함.
+        }
+    }
+
+    public boolean delNode(T isData){
+        if(head == null){
+            return false;
+        }else{
+            Node<T> node = this.head;
+            if(node.data == isData){
+                this.head = this.head.next;
+                return true;
+            }else{
+                while(node.next != null){
+                    if(node.next.data == isData){
+                        node.next = node.next.next; // 노드를 삭제하게 되면 그 다음 노드를 가르키도록 하는 코드.( 1->3 2x)
+                        return true;
+                    }
+                    node = node.next;
+                }
+                return false;
+            }
         }
     }
 }
--- src/test/java/ex03/LinkedListClassTest.java
+++ src/test/java/ex03/LinkedListClassTest.java
@@ -1,5 +1,6 @@
 package ex03;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,11 +12,53 @@ class LinkedListClassTest {
     void LinkedListTest(){
         SingleLinkedList<Integer> linkedList = new SingleLinkedList<Integer>();
         linkedList.addNode(1);
+        linkedList.addNode(2);
+        linkedList.addNode(3);
 
-        System.out.println(linkedList.head); // -> 주솟값
-        System.out.println(linkedList.head.data); // -> 1
-        System.out.println(linkedList.head.next); // null
+//        System.out.println(linkedList.head); // -> 주솟값
+//        System.out.println(linkedList.head.data); // -> 1
+//        System.out.println(linkedList.head.next); // null
+
+        // 1 뒤에 5 넣어 보기
+        linkedList.addNodeInside(5,1);
+//        linkedList.printAll(); // 1 5 2 3
+
+        // 3(맨 마지막) 뒤에 6 넣어보기
+        linkedList.addNodeInside(6,3);
+//        linkedList.printAll(); // 1 5 2 3 6
+
+        // 없는 데이터의 앞에 끼워 넣어보기
+        linkedList.addNodeInside(7,20);
+        linkedList.printAll(); // 7이 맨 뒤로 들어감.
+
+
 
     }
+
+    @Test
+     void delNodeTest(){
+        SingleLinkedList<Integer> linkedList = new SingleLinkedList<Integer>();
+        linkedList.addNode(1);
+        linkedList.addNode(2);
+        linkedList.addNode(3);
+        linkedList.addNode(4);
+        linkedList.addNode(5);
+
+        // 중간 노드 삭제
+        linkedList.delNode(3); // 1,2,4,5
+
+
+        // 헤드 삭제
+        linkedList.delNode(1); // 2,4,5
+
+
+        // 맨 마지막 노드 삭제
+        linkedList.delNode(5); // 2,4
+
+        // 없는 노드 삭제
+        Assertions.assertTrue(linkedList.delNode(20)); // false -> 테스트 실패
+
+        linkedList.printAll();
+     }
 
 }
